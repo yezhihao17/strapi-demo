7ee8080d1fe02ec62a5cef91fb1b6e7e2901409d
yezhihao17 <yezhihao17@163.com>
2020-11-20 11:11:25 +0800

    initail commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c069352d6185b05372c0e06b0276daf5'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '0462a5d1-1a1a-4ba2-80a3-8f940205a083'
+};
